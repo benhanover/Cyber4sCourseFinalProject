1eeaab20199c78069ba12b4d5cc232897a540649
Gorgula <68461592+DGorgula@users.noreply.github.com>
2021-06-23 14:23:46 +0300

    Delete server/dist directory

--- server/dist/js/utils/mongo/schema/roomSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const { Schema } = require("mongoose");
-const schema = new Schema({
-    name: String,
-    id: String,
-    participants: [String],
-});
-module.exports.roomSchema = schema;
