eee70a7c4182ee5935ccd52258c9fbda7146ac33
amit751 <77381301+amit751@users.noreply.github.com>
2021-07-08 19:33:18 +0300

    commit cansel+ choose for me  +enums+react child err fix

--- client/src/components/VideoRoom/VideoRoom.tsx
+++ client/src/components/VideoRoom/VideoRoom.tsx
@@ -148,12 +148,14 @@ function VideoRoom() {
               Share Screen
             </button>
           )}
-          {chooseNewHost && (
+          {chooseNewHost && room.participants.length > 1 && (
             <div className="choose-host-box">
-              {room.participants.map((participant: any) => {
+              <p>plese chose a new host for the room</p>
+              {room.participants.map((participant: any, index: number) => {
                 if (participant.user._id !== user._id) {
                   return (
                     <div
+                      key={index}
                       className="host-choise"
                       onClick={() => {
                         leaveRoom(
@@ -179,6 +181,31 @@ function VideoRoom() {
                   );
                 }
               })}
+              <button
+                onClick={() => {
+                  SetchooseNewHost(false);
+                }}
+              >
+                cansel
+              </button>
+              <button
+                onClick={() => {
+                  leaveRoom(
+                    roomId,
+                    peerId,
+                    serverSocket,
+                    videos,
+                    user,
+                    myStream,
+                    room,
+                    enums.defaultHost
+                  );
+                  SetchooseNewHost(false);
+                  history.push("/lobby");
+                }}
+              >
+                choose for me
+              </button>
             </div>
           )}
           <button
@@ -194,20 +221,33 @@ function VideoRoom() {
   //functions:
   /*-------------------------------------------------------------------------------------*/
   function handleLeaveBuuton() {
+    console.log(room.participants, ":users in room");
     if (user._id !== room.host.userId) {
-      leaveRoom(roomId, peerId, serverSocket, videos, user, myStream, room);
+      console.log(1);
+
+      leaveRoom(
+        roomId,
+        peerId,
+        serverSocket,
+        videos,
+        user,
+        myStream,
+        room,
+        enums.dontChangeHost
+      );
       history.push("/lobby");
     } else if (
       room.participants.length === 1 &&
       room.participants[0].user._id === user._id
     ) {
+      console.log(2);
       serverSocket.send(
         JSON.stringify({
           type: "delete room",
           message: room._id,
         })
       );
-      //make shur there are no open calls
+      //make shure there are no open calls
       videos.forEach((video: any) => {
         video.call.close();
       });
@@ -217,6 +257,7 @@ function VideoRoom() {
       });
       history.push("/lobby");
     } else {
+      console.log(3);
       SetchooseNewHost(true);
     }
   }
