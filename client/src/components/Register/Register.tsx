43f6179bc33e2e903da7baddcff9f939e9eb7275
amit751 <77381301+amit751@users.noreply.github.com>
2021-07-09 18:36:06 +0300

    regisrter style

--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,23 +1,23 @@
 // import libraries
-import React, { useRef } from 'react';
-import Cookies from 'js-cookie';
-import { useDispatch} from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { useHistory } from 'react-router-dom';
-import './Register.css';
+import React, { useRef } from "react";
+import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { useHistory } from "react-router-dom";
+import "./Register.css";
 
 // react shit
-import { wsActionCreator } from '../../state';
+import { wsActionCreator } from "../../state";
 
 // import functions
 
-import {register} from './functions'
+import { register } from "./functions";
 
 const Register: React.FC = () => {
   const dispatch = useDispatch();
   const history = useHistory();
-  const { setUser } = bindActionCreators({...wsActionCreator}, dispatch);
-  
+  const { setUser } = bindActionCreators({ ...wsActionCreator }, dispatch);
+
   // refs
   const usernameRef = useRef<HTMLInputElement | null>(null);
   const nameRef = useRef<HTMLInputElement | null>(null);
@@ -34,21 +34,22 @@ const Register: React.FC = () => {
   // const hobbysRef = useRef<HTMLTextAreaElement | null>(null);
   // const relationshipStatusRef = useRef<HTMLSelectElement | null>(null);
 
-
-
   return (
     <div className="register-container">
-
-      <button className='login-btn button' onClick={() => history.push('/')}>Login</button>
-    <form className="register-form" onSubmit={handleSubmit}>
-      <input ref={usernameRef} placeholder="Username" />
-      <input ref={nameRef}  placeholder="First Name"/>
-      <input ref={lastNameRef} placeholder="Last Name"/>
-      <input type='date' ref={dateRef} placeholder="BirthDate"/>
-      <input type='email' ref={emailRef} placeholder="Email"/>
-      <input type='text' ref={passwordRef} placeholder="Password" />
-      <input type='submit' className="button" value="Register" />
-      {/* <br />
+      <div className="rectangle-shape"></div>
+      <div className="triangle-shape"></div>
+      <button className="login-btn button" onClick={() => history.push("/")}>
+        Login
+      </button>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <input ref={usernameRef} placeholder="Username" />
+        <input ref={nameRef} placeholder="First Name" />
+        <input ref={lastNameRef} placeholder="Last Name" />
+        <input type="date" ref={dateRef} placeholder="BirthDate" />
+        <input type="email" ref={emailRef} placeholder="Email" />
+        <input type="text" ref={passwordRef} placeholder="Password" />
+        <input type="submit" className="button submit-btn" value="Register" />
+        {/* <br />
       <br />
       <br />
       <input type='file' ref={profileImageRef} accept="image/*" />
@@ -69,27 +70,47 @@ const Register: React.FC = () => {
       <input type='checkbox' value='16:00-00:00' id='evening' />
       <label>00:00-08:00</label>
     <input type='checkbox' value='00:00-08:00' id='night' /> */}
-    </form>
-</div>
+      </form>
+    </div>
   );
 
   // functions
-/*-----------------------------------------------------------------------------------------------------------------*/
-function cleanup(): void {
-  if(!(usernameRef?.current && nameRef?.current && lastNameRef?.current && dateRef?.current && emailRef?.current && passwordRef?.current)) return;
-  usernameRef.current.value = '';
-  nameRef.current.value = '';
-  lastNameRef.current.value = '';
-  dateRef.current.value = '';
-  emailRef.current.value = '';
-  passwordRef.current.value = '';
-}
+  /*-----------------------------------------------------------------------------------------------------------------*/
+  function cleanup(): void {
+    if (
+      !(
+        usernameRef?.current &&
+        nameRef?.current &&
+        lastNameRef?.current &&
+        dateRef?.current &&
+        emailRef?.current &&
+        passwordRef?.current
+      )
+    )
+      return;
+    usernameRef.current.value = "";
+    nameRef.current.value = "";
+    lastNameRef.current.value = "";
+    dateRef.current.value = "";
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+  }
 
-/*-----------------------------------------------------------------------------------------------------------------*/
+  /*-----------------------------------------------------------------------------------------------------------------*/
   async function handleSubmit(e: React.SyntheticEvent): Promise<void> {
     e.preventDefault();
-    
-    if(!(usernameRef?.current && nameRef?.current && lastNameRef?.current && dateRef?.current && emailRef?.current && passwordRef?.current)) return;
+
+    if (
+      !(
+        usernameRef?.current &&
+        nameRef?.current &&
+        lastNameRef?.current &&
+        dateRef?.current &&
+        emailRef?.current &&
+        passwordRef?.current
+      )
+    )
+      return;
     const username: string | undefined = usernameRef.current?.value;
     const firstName: string | undefined = nameRef.current?.value;
     const lastName: string | undefined = lastNameRef.current?.value;
@@ -100,14 +121,21 @@ function cleanup(): void {
     // clean up
     cleanup();
     try {
-      const { data: response } = await register(username, firstName, lastName, birthDate, email, password);
-      Cookies.set('accessToken', response.accessToken);
-      Cookies.set('refreshToken', response.refreshToken);
-      setUser(response.user)
+      const { data: response } = await register(
+        username,
+        firstName,
+        lastName,
+        birthDate,
+        email,
+        password
+      );
+      Cookies.set("accessToken", response.accessToken);
+      Cookies.set("refreshToken", response.refreshToken);
+      setUser(response.user);
     } catch (e) {
       console.log("couldn't register", e);
     }
-  };
+  }
 };
 
 export default Register;
