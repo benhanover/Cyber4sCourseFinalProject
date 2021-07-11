5f347ddcdce486daaa23f337462912b9108ed59f
amit751 <77381301+amit751@users.noreply.github.com>
2021-07-11 11:50:21 +0300

    landing page

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -26,11 +26,13 @@ const Login: React.FC = () => {
     <div className="login-component">
      <div className="rectangle-shape">
 
-      <h1>Roomie</h1>
+      <h1>Room-in</h1>
       <div className="about-container">
-      <div className="p1">al fauna pas paucituberculatans[14] and sparassodonts[13][15][16] Large opossums like Didelphis show a pattern of gradually increasing in size over geologic time as sparassodont diversity declined.[15][16] Several groups of opossums, including Thylophorops, Thylatheridium, Hyperdidelphys, and sparassocynins developed carnivorous adaptations during the late Miocene-Pliocene, prior to the arrival of carnivorans in South America. Most of these groups with the exception of Lutreolina are now extinct.</div>
-      <div className="p2">al Priums were occupied by other groups of metatherians such as paucituberculatans[14] and sparassodonts[13][15][16] Large opossums like Didelphis show a pattern of gradually increasing in size over geologic time as sparassodont diversity declined.[15][16] Several groups of opossums, including Thylophorops, Thylatheridium, Hyperdidelphys, and sparassocynins developed carnivorous adaptations during the late Miocene-Pliocene, prior to the arrival of carnivorans in South America. Most of these groups with the exception of Lutreolina are now extinct.</div>
-      <div className="p3">al   as paucituberculatans[14] and sparassodonts[13][15][16] Large opossums like Didelphis show a pattern of gradually increasing in size over geologic time as sparassodont diversity declined.[15][16] Several groups of opossums, including Thylophorops, Thylatheridium, Hyperdidelphys, and sparassocynins developed carnivorous adaptations during the late Miocene-Pliocene, prior to the arrival of carnivorans in South America. Most of these groups with the exception of Lutreolina are now extinct extinct.</div>
+      <div className="p1">hi there and welcome, where ever you are just room in! </div>
+      <div className="p2">if you looking for a study evnironment , a place to learn and concentrate you arived.  we offer you a Pleasant atmosphere that encourages you to enter into  optimal state of learning. Overcome the difficulty together with other people like you who gather for the purpose of learning.you can use the help of other students , see them on video, communicate ,  consult, share your screen with them and more.
+       </div>
+      <div className="p3"> all you need to do is just connect, find a room that matches your theme, or follow your curiosity and join a room on a new subject , get to know people who familiar with it. OR open your one room with  a subject of your choice. You can control the number of participants in the room, decide who has access to it, and make it private.
+To know a little more   about the students in the room you can view their profile.  so lets get started: register, edit your profile and room in</div>
       </div>
       </div>
     <div className="triangle-shape"></div>
