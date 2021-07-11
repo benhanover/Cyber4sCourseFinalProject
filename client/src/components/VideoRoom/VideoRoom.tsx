4844acd74d7ab51ab17096a20768a63d8e8c8c3a
amit751 <77381301+amit751@users.noreply.github.com>
2021-07-11 16:49:51 +0300

    user video

--- client/src/components/VideoRoom/VideoRoom.tsx
+++ client/src/components/VideoRoom/VideoRoom.tsx
@@ -57,6 +57,7 @@ function VideoRoom() {
   const [videos, setVideos] = useState<any>([]);
   const [myStream, setMyStream] = useState<any>();
   const [myVideoIsOn, setMyVideoIsOn] = useState<any>(true);
+  const [myAudioIsOn, setMyAudioIsOn] = useState<any>(true);
   const roomId = location.search.slice(8);
   const [noUserDevices, SetNoUserDevices] = useState<boolean>(false);
   const [chooseNewHost, SetchooseNewHost] = useState<any>(null);
@@ -89,9 +90,18 @@ function VideoRoom() {
   return (
     <>
       {videos && room && (
+        
         <div className="video-room">
+          <button className="close-room-btn"
+            onClick={() => closeRoom(serverSocket, roomId, room.isClosed)}
+          >
+          {isClosedButtonName}
+          </button>
+          <button className="leave-button" onClick={handleLeaveBuuton}>
+            Leave
+          </button>
           {noUserDevices && (
-            <div>
+            <div className="media-premision-err-div">
               <h1 style={{ color: "white" }}>
                 you must give premition to media devices, at list audio
               </h1>
@@ -118,6 +128,7 @@ function VideoRoom() {
             );
           })}
           {myStream && (
+            <div className="self-video-box">
             <UserVideo
               muted={true}
               stream={myStream}
@@ -125,30 +136,64 @@ function VideoRoom() {
               username={user.username}
               isVideoOn={myVideoIsOn}
             />
-          )}
-          <button className="leave-button" onClick={handleLeaveBuuton}>
-            Leave
-          </button>
-          <button
-            className="self-mute-button"
-            onClick={() => selfMuteToggle(myStream)}
-          >
-            Mute
-          </button>
-          <button
-            className="stop-self-video-button"
-            onClick={() => setMyVideoIsOn(selfVideoToggle(myStream))}
-          >
-            Stop Video
-          </button>
+            <div className="video-buttons-div">
+          {  myAudioIsOn? 
+          <div onClick={() => setMyAudioIsOn(selfMuteToggle(myStream))} className="svg-container svg-mute-container">
+          <svg className="self-mute-button bi bi-mic-fill"
+             xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="white"  viewBox="0 0 16 16">
+            <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z"/>
+            <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
+          
+          </svg>
+          <p>mute</p>
+          </div>
+          :
+          <div onClick={() => setMyAudioIsOn(selfMuteToggle(myStream))} className="svg-container svg-mute-container">
+          <svg className="self-mute-button bi bi-mic-mute-fill" xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="white"  viewBox="0 0 16 16">
+            <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4.02 4.02 0 0 0 12 8V7a.5.5 0 0 1 1 0v1zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a4.973 4.973 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4zm3-9v4.879L5.158 2.037A3.001 3.001 0 0 1 11 3z"/>
+            <path d="M9.486 10.607 5 6.12V8a3 3 0 0 0 4.486 2.607zm-7.84-9.253 12 12 .708-.708-12-12-.708.708z"/>
+          </svg>
+          <p>un-mute</p>
+          </div>
+        }
+         { myVideoIsOn? 
+        <div onClick={() => setMyVideoIsOn(selfVideoToggle(myStream))} className=" svg-container svg-video-container">
+         <svg  className="video-svg bi bi-camera-video-fill"xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="white" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z"/>
+         </svg>
+          <p>stop video</p>
+        </div>
+          :
+          <div onClick={() => setMyVideoIsOn(selfVideoToggle(myStream))} className=" svg-container svg-video-container">
+         <svg className="video-svg bi bi-camera-video-off-fill" xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="white"  viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M10.961 12.365a1.99 1.99 0 0 0 .522-1.103l3.11 1.382A1 1 0 0 0 16 11.731V4.269a1 1 0 0 0-1.406-.913l-3.111 1.382A2 2 0 0 0 9.5 3H4.272l6.69 9.365zm-10.114-9A2.001 2.001 0 0 0 0 5v6a2 2 0 0 0 2 2h5.728L.847 3.366zm9.746 11.925-10-14 .814-.58 10 14-.814.58z"/>
+         </svg>
+          <p>start video</p>
+          </div>
+        }
+       
           {myStream?.getVideoTracks()[0] && (
-            <button
-              className="share-screen-button"
-              onClick={async () => await shareScreen(videos, myStream)}
-            >
-              Share Screen
-            </button>
+            
+            <div onClick={async () => await shareScreen(videos, myStream)} className="svg-container svg-sharescreen-container">
+            <svg className="svg-sharescreen bi bi-upload " xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="white"  viewBox="0 0 16 16">
+              <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+              <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
+            </svg>
+            <p>share-screen</p>
+          </div>
+            // <button
+            //   className="share-screen-button"
+            //   onClick={async () => await shareScreen(videos, myStream)}
+            // >
+            //   Share Screen
+            // </button>
           )}
+          </div>
+          </div>
+          )}
+          
+         
+          
           {chooseNewHost && room.participants.length > 1 && (
             <div className="choose-host-box">
               <p>plese chose a new host for the room</p>
@@ -209,11 +254,7 @@ function VideoRoom() {
               </button>
             </div>
           )}
-          <button
-            onClick={() => closeRoom(serverSocket, roomId, room.isClosed)}
-          >
-            {isClosedButtonName}
-          </button>
+       
         </div>
       )}
     </>
