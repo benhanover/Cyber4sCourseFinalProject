257fa57cd63ba8c76dd0b710fbe261ffcbff503d
DGorgula <gil@dankner.org>
2021-07-12 13:15:25 +0300

    client/src/components/AccountSettings
    Signed-off-by: DGorgula <gil@dankner.org>

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -30,18 +30,19 @@ const Navbar: React.FC<Iprops> = ({setDisplayAccountSettings, displayAccountSett
       <Link className="logo bold" to='/lobby'><h1>Room-In</h1></Link>
       <div className="options">
         <SearchUser />
-        <div className="profile-menu-container" onMouseLeave={()=>setProfileMenu(false)}>
+        <div className="profile-menu-container">
           <img className="profile-menu-button" src={user.profile.img} onClick={toggleProfileMenu}/>
             {profileMenu &&
           
-            <ul className="profile-menu" >
-              <li className='cursor'>
-                <Link to='/my-profile'>My Profile</Link>
+            <ul className="profile-menu dropdown_menu"  onMouseLeave={()=>setProfileMenu(false)} onClick={()=>setProfileMenu(false)} >
+            <li className="cursor">
+            
+                <Link to='/my-profile' color='black'>Profile</Link>
               </li>
               <li className='cursor'>
                 <span onClick={() => setDisplayAccountSettings(!displayAccountSettings)}>Account Settings</span>
               </li>
-              <li className='cursor'>
+            <li className='cursor' >
                 <LogoutButton />
               </li>
               
