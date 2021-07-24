5535e9a1467e040f267acf92781354ce2252b94a
DGorgula <gil@dankner.org>
2021-07-24 17:30:47 +0300

    demo created
    Signed-off-by: DGorgula <gil@dankner.org>

--- client/src/utils/enums.ts
+++ client/src/utils/enums.ts
@@ -1,8 +1,8 @@
 export enum enums {
   // baseUrl = "http://localhost:4000",
   // wsUrl = "localhost:4000",
-  baseUrl = "http://10.0.0.21:4000",
-  wsUrl = "10.0.0.21:4000",
+  baseUrl = "http://192.168.43.150:4000",
+  wsUrl = "192.168.43.150:4000",
 
   defaultHost = "chooseforme",
   dontChangeHost = "dontChangHost",
