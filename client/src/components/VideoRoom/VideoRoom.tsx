6f3474b9e753ae672018486a389ddd984a6b6e00
DGorgula <gil@dankner.org>
2021-07-29 08:38:20 +0300

    typo
    Signed-off-by: DGorgula <gil@dankner.org>

--- client/src/components/VideoRoom/VideoRoom.tsx
+++ client/src/components/VideoRoom/VideoRoom.tsx
@@ -228,7 +228,7 @@ function VideoRoom() {
                 }}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
                 <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/>
               </svg></div>
-              <p className="choose-host-p">plese chose a new host for the room</p>
+              <p className="choose-host-p">Please chose a new host for the room</p>
               {room.participants.map((participant: any, index: number) => {
                 if (participant.user._id !== user._id) {
                   return (
